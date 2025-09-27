ca040e5e20cb55817bea1f33c052c2ab346e84cc
Garv Raj <154749848+GitWithGarv@users.noreply.github.com>
2025-09-27 04:46:46 +0000

    paasing objects as parameters

--- complex.java
+++ complex.java
@@ -10,6 +10,10 @@ class Complex{
     void display(){
         System.out.println(real + " + " + img + "i");
     }
+    void add(Complex c1, Complex c2){
+        this.real = c1.real + c2.real;
+        this.img = c1.img + c2.img;
+    }
 }
 
 public class complex {
@@ -21,6 +25,14 @@ public class complex {
         c1.img = sc.nextInt();
         c1.getdata(c1.real, c1.img);
         c1.display();
+        Complex c2 = new Complex();
+        c2.real = sc.nextInt();
+        c2.img = sc.nextInt();
+        c2.getdata(c2.real, c2.img);
+        c2.display();
+        Complex c3 = new Complex();
+        c3.add(c1, c2);
+        c3.display();
         sc.close();
     }
 }
